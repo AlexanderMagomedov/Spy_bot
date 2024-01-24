72f0225b745626f154291f3d9dc0caa19b4f0ae5
ÐAlexander Magomedov <sistem4ik@mail.ru>
2024-01-24 14:19:13 +0500

    Add /about_me logic

--- telebot/handlers/user_handlers.py
+++ telebot/handlers/user_handlers.py
@@ -1,16 +1,35 @@
-from aiogram import Router
+from aiogram import F, Router
 from aiogram.filters import Command, CommandStart
 from aiogram.types import Message
-
-from telebot.keyboards.keyboards import create_menu_keyboard
+from aiogram.types import CallbackQuery
+from telebot.keyboards.keyboards import create_menu_keyboard, create_back_keyboard
 from telebot.lexicon.lexicon_ru import LEXICON_RU
 
 
 router = Router()
 
 
-# Этот хэндлер будет срабатывать на команду "/start"
-# и отправлять ему приветственное сообщение показывать кнопки главного меню
+# Эт X срабатывает на команду "/start" отправлять приветственное сообщение показывая кнопки главного меню
 @router.message(CommandStart())
 async def process_start_command(message: Message):
     await message.answer(LEXICON_RU[message.text], reply_markup=create_menu_keyboard())
+
+
+# Эт X срабатывает на нажатие инлайн-кнопки "Назад" и возвращать в главное меню
+@router.callback_query(F.data == '/start')
+async def process_start(callback: CallbackQuery):
+    await callback.message.edit_text(text=LEXICON_RU['/start'], reply_markup=create_menu_keyboard())
+    await callback.answer()
+
+
+# Эт X срабатывает на команду "/about_me" отправляет сообщение о нас и кнопку "назад"
+@router.message(Command(commands='about_me'))
+async def process_about_me_command(message: Message):
+    await message.answer(LEXICON_RU[message.text], reply_markup=create_back_keyboard('/start'))
+
+
+# Эт X срабатывает на нажатие инлайн-кнопки "Обо мне" в главном меню показывает сообщение и кнопку "назад"
+@router.callback_query(F.data == '/about_me')
+async def process_about_me(callback: CallbackQuery):
+    await callback.message.edit_text(text=LEXICON_RU['/about_me'], reply_markup=create_back_keyboard('/start'))
+    await callback.answer()
--- telebot/keyboards/keyboards.py
+++ telebot/keyboards/keyboards.py
@@ -2,7 +2,7 @@ from aiogram import Bot
 from aiogram.types import BotCommand
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
-from telebot.lexicon.lexicon_ru import LEXICON_COMMANDS
+from telebot.lexicon.lexicon_ru import LEXICON_COMMANDS, LEXICON_RU
 
 
 # Функция создания инлайн кнопок главного меню
@@ -11,8 +11,19 @@ def create_menu_keyboard() -> InlineKeyboardMarkup:
     kb_builder = InlineKeyboardBuilder()
     # Наполняем клавиатуру кнопками-закладками в порядке возрастания
     kb_builder.row(
-        InlineKeyboardButton(text=LEXICON_COMMANDS['game'], callback_data='game'),
-        InlineKeyboardButton(text=LEXICON_COMMANDS['rules'], callback_data='rules'),
-        InlineKeyboardButton(text=LEXICON_COMMANDS['about_me'], callback_data='about_me'), width=1
+        InlineKeyboardButton(text=LEXICON_COMMANDS['/game'], callback_data='/game'),
+        InlineKeyboardButton(text=LEXICON_COMMANDS['/rules'], callback_data='/rules'),
+        InlineKeyboardButton(text=LEXICON_COMMANDS['/about_me'], callback_data='/about_me'), width=1
     )
+    return kb_builder.as_markup()
+
+
+# Функция создания инлайн кнопки "Назад"
+def create_back_keyboard(arg) -> InlineKeyboardMarkup:
+    # Создаем объект клавиатуры
+    kb_builder = InlineKeyboardBuilder()
+    # Наполняем клавиатуру кнопками-закладками в порядке возрастания
+    kb_builder.row(InlineKeyboardButton(
+        text=LEXICON_RU['/back'],
+        callback_data=arg))
     return kb_builder.as_markup()
